34a51c3577798951f6e8f94223c7e9bb43d625d0
Jean-Marie Dormoy <jean-marie.dormoy@etu.unice.fr>
2020-06-11 10:27:28 +0200

    #11 added Makefile for GeneticBotTest.java

---
+++ Test/GeneticBotTest.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class GeneticBotTest {
+    @Test
+    public void testConcatenate() {
+        assertEquals("onetwo", "onetwo");
+    }
+}
